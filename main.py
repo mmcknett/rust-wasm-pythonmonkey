5c7c3ab28bdd6c575a394006681b56085aba9862
Matt McKnett <mmcknett@gmail.com>
2023-09-08 15:10:44 -0700

    Add a `sub` method

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import pythonmonkey as pm
 my_lib = pm.require('./pkg/adder')
 
 print(my_lib.add(5, 8))
+print(my_lib.sub(1, 2))
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,11 @@ pub fn add(left: u32, right: u32) -> u32 {
     left + right
 }
 
+#[wasm_bindgen]
+pub fn sub(left: i32, right: i32) -> i32 {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
